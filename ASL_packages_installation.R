7d6143ce5b7ab1a91298b4f772b11f4b8c53e5a6
Ksenija Mišić <90040722+ksenijamisic@users.noreply.github.com>
2026-04-22 07:27:19 +0200

    Add files via upload

---
+++ ASL_packages_installation.R
@@ -0,0 +1,24 @@
+# ASL_packages_installation
+install.packages(c(
+  "stats",
+  "psych",
+  "tidyverse",
+  "car",
+  "emmeans",
+  "afex",
+  "readxl",
+  "lme4",
+  "Hmisc"
+))
+
+
+
+library(stats)
+library(psych)
+library(tidyverse)
+library(car)
+library(emmeans)
+library(afex)
+library(readxl)
+library(lme4)
+library(Hmisc)
